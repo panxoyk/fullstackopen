65f16245b1f4544830ea459918f300d412dd1cb0
panncho <francisco.castilloh@usm.cl>
2023-01-26 23:14:41 -0300

    Exercises 1.1 1.2

---
+++ part1/course_info/src/components/Content.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Part from './Part';
+
+const Content = (props) => {
+    return (
+        <>
+            <Part part={props.content[0][0]} exercises={props.content[0][1]} />
+            <Part part={props.content[1][0]} exercises={props.content[1][1]} />
+            <Part part={props.content[2][0]} exercises={props.content[2][1]} />
+        </>
+               
+    );
+};
+
+export default Content;
