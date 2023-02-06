eea714bcdf9287c0ef2b7f551aa7d0f97875098f
panncho <francisco.castilloh@usm.cl>
2023-02-05 23:27:10 -0300

    Exercises 4.3 4.4 4.5 4.6 4.7

---
+++ part4/bloglist/backend/utils/list_helper.js
@@ -0,0 +1,54 @@
+const _ = require('lodash')
+
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, curr) => sum + curr.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    let count = 0
+    blogs.map(blog => {
+        if (blog.likes > count) {
+            count = blog.likes
+        }
+    })
+    const favoriteBlog = blogs.find(blog => blog.likes === count)
+
+    return blogs.length === 0
+        ? null
+        : favoriteBlog
+}
+
+const mostBlogs = (blogs) => {
+    const authorWithBlogNumber = _.countBy(blogs, 'author')
+    const maxValue = Math.max(...Object.values(authorWithBlogNumber))
+    const maxIndex = Object.keys(authorWithBlogNumber).find(key => authorWithBlogNumber[key] === maxValue)
+
+    return blogs.length === 0
+        ? null
+        : { author: maxIndex, blogs: maxValue }
+}
+
+const mostLikes = (blogs) => {
+    let authorWithMostLikes = {}
+    
+    blogs.forEach(blog => {
+        if (blog.author in authorWithMostLikes) {
+            authorWithMostLikes[blog.author] += blog.likes
+        } else {
+            authorWithMostLikes[blog.author] = blog.likes
+        }
+    })
+
+    const maxValue = Math.max(...Object.values(authorWithMostLikes))
+    const maxIndex = Object.keys(authorWithMostLikes).find(key => authorWithMostLikes[key] === maxValue)
+
+    return blogs.length === 0
+        ? null
+        : { author: maxIndex, likes: maxValue }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
