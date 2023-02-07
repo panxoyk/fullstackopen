42b48696eb7f5e51d5fd44bb9f7549cdb0faa984
panncho <francisco.castilloh@usm.cl>
2023-02-07 19:17:57 -0300

    Exercises 3.13 3.14

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -14,4 +14,26 @@ blogsRouter.post('/', async (request, response) => {
     response.status(201).json(savedBlog)
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+        request.params.id, blog, { new: true, runValidators: true, context: 'query' }
+    )
+
+    response.json(updatedBlog)
+})
+
 module.exports = blogsRouter
