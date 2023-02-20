fbd96b589ac451005b1f6bdeab279ba99424d220
panncho <francisco.castilloh@usm.cl>
2023-02-20 18:37:02 -0300

    Exercises 6.10 6.11 6.12 1.13

--- part6/anecdotes-redux/src/components/AnecdoteForm.js
+++ part6/anecdotes-redux/src/components/AnecdoteForm.js
@@ -1,14 +1,19 @@
 import { useDispatch } from 'react-redux'
 import { create } from '../reducers/anecdoteReducer'
+import { successNotify, nullNotify } from '../reducers/notificationReducer'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         dispatch(create(content))
+        dispatch(successNotify(`Added ${content}`))
         event.target.anecdote.value = ''
+        setTimeout(() => {
+            dispatch(nullNotify())
+        }, 5000)
     }
 
     return (
