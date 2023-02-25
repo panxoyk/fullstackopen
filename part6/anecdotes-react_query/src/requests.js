d343694769e5fedb4faa6dd6383981e61156cdb1
panncho <francisco.castilloh@usm.cl>
2023-02-24 23:34:09 -0300

    Exercises 6.20 6.21 6.22

---
+++ part6/anecdotes-react_query/src/requests.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAnecdotes = () => axios.get(baseUrl).then(response => response.data)
+
+export const createAnecdote = (createdAnecdote) => axios.post(baseUrl, createdAnecdote).then(response => response.data)
+
+export const updateAnecdote = (updatedAnecdote) => axios.put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote).then(response => response.data)
